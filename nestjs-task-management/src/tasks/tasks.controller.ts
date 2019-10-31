29e6df0baf0a67e7fe876bc45803062a424c77a7
Gabriel Coelho <gabriellvs60@gmail.com>
2019-10-31 11:40:30 -0300

    Getting Tasks e Defining Model Tasks
    Get tasks
    e
    definindo modelo de tasks

--- nestjs-task-management/src/tasks/tasks.controller.ts
+++ nestjs-task-management/src/tasks/tasks.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-
+import {Task} from './task.model';
 @Controller('tasks')
 export class TasksController {
-    constructor(taskService: TasksService){}
+    constructor(private taskService: TasksService){}
+
+    @Get()
+    getAllTasks() : Task[]{
+       return this.taskService.getAllTasks();
+    }
 }
--- nestjs-task-management/src/tasks/tasks.service.ts
+++ nestjs-task-management/src/tasks/tasks.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import{Task} from './task.model';
 
+// é do serviço, a responsabilidade de manipular todas as tasks
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    private tasks :Task[] = [];
+
+    getAllTasks(): Task[]{
+        return this.tasks;
+    }
+}
